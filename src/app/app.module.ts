3f7467970d58d7ee6cdb4c89548963d29906fd5f
amir-ghaderian@github.com <79098240+amir-ghaderian@users.noreply.github.com>
2023-11-30 12:08:51 +0330

    create header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    CarouselComponent
+    CarouselComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
